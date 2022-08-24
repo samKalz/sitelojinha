17315ce4779f0d0207a2fd7d32959f9b95c9b276
samKalz <112024344+samKalz@users.noreply.github.com>
2022-08-24 17:35:28 -0300

    commit inicial

---
+++ custom.js
@@ -0,0 +1,5 @@
+// Enviar a imagem salva no sevidor do JavaScript para o HTML
+document.getElementById("imagem").innerHTML = "<img src='imagem/ lojapc.jpg' width='15'>";
+document.getElementById("imagem").innerHTML = "<img src='imagem/ ram.jpg' width='15'>";
+document.getElementById("imagem").innerHTML = "<img src='imagem/ ssd.jpg' width='15'>";
+document.getElementById("imagem").innerHTML = "<img src='imagem/ aircoler.jpg' width='15'>";
